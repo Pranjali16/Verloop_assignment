b9c16c7b3654ebbf8f45a1f64989812d5841a56e
pranjali <Pranjali16@users.noreply.github.com>
2020-08-29 23:54:39 +0530

    master branch commit
    git add .gitignore

---
+++ verloop_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class VerloopAppConfig(AppConfig):
+    name = 'verloop_app'
---
+++ verloop_app/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.postgres.fields import ArrayField
+
+
+class BaseModel(models.Model):
+
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Story(BaseModel):
+    """ Story Model"""
+    title = models.CharField(max_length=500, blank=True, null=True)
+    sentences = ArrayField(models.CharField(max_length=500), blank=True, null=True)
+    paragraphs = ArrayField(models.CharField(max_length=1000), blank=True, null=True)
+    offset = models.PositiveIntegerField(blank=True)
+    count = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return "%s %s" % (self.title, self.created_at)
